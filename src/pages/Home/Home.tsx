8fd63c372ac84111b1b691a78b67712387ddcb19
Gabriel Freitas <bielfreitas@alu.ufc.br>
2023-07-04 16:28:53 -0300

    refactor: home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,26 @@
 import "./Home.scss";
-import React from "react";
+import React, { useState } from "react";
 import Header from "components/Header/Header";
 import DocumentVerifiedIcon from "assets/icons/documentVerified.svg";
 import FacialVerifIcon from "assets/icons/facialVerfication.svg";
 import MediaCompIcon from "assets/icons/mediaComparation.svg";
 import BlogIcon from "assets/icons/blogLogo.svg";
 import { BiRightArrowAlt } from "react-icons/bi";
+import { Input } from "antd";
 
 function Home() {
+  const [rotatedCards, setRotatedCards] = useState<number[]>([]);
+  const rotateFeatureCard = (cardId: number) => {
+    const index = rotatedCards.indexOf(cardId);
+    if (index === -1) {
+      setRotatedCards([...rotatedCards, cardId]);
+    } else {
+      const newArray = rotatedCards.slice();
+      newArray.splice(index, 1);
+      setRotatedCards(newArray);
+    }
+  };
+
   return (
     <React.Fragment>
       <Header />
@@ -23,37 +36,70 @@ function Home() {
             </p>
           </div>
           <div className="feature-cards">
-            <div className="feature-card">
+            <div
+              className={`feature-card ${
+                rotatedCards.includes(1) ? "rotated" : ""
+              }`}
+            >
               <img src={DocumentVerifiedIcon} alt="documentos verificados" />
               <p className="feature-card-title">
                 Acompanhantes com <strong>documentos verificados</strong>
               </p>
-              <BiRightArrowAlt size={30} />
+              <p className="feature-card-description">
+                <strong>Todo profissional</strong> passa por uma verificação de
+                seus documentos
+              </p>
+              <BiRightArrowAlt size={30} onClick={() => rotateFeatureCard(1)} />
             </div>
-            <div className="feature-card">
+            <div
+              className={`feature-card ${
+                rotatedCards.includes(2) ? "rotated" : ""
+              }`}
+            >
               <img src={FacialVerifIcon} alt="documentos verificados" />
               <p className="feature-card-title">
                 Verificação <strong>facial</strong>
               </p>
-              <BiRightArrowAlt size={30} />
+              <p className="feature-card-description">
+                O Rosto precisa ser <strong>sempre igual</strong> ao do
+                documento registrado.
+              </p>
+              <BiRightArrowAlt size={30} onClick={() => rotateFeatureCard(2)} />
             </div>
-            <div className="feature-card">
+            <div
+              className={`feature-card ${
+                rotatedCards.includes(3) ? "rotated" : ""
+              }`}
+            >
               <img src={MediaCompIcon} alt="documentos verificados" />
               <p className="feature-card-title">
                 Mídia de
                 <br />
                 <strong>comparação 360°</strong>
               </p>
-              <BiRightArrowAlt size={30} />
+              <p className="feature-card-description">
+                Vídeo verificado com corpo e rosto. Precisa{" "}
+                <strong>ser o mesmo</strong> do documento registrado.
+              </p>
+              <BiRightArrowAlt size={30} onClick={() => rotateFeatureCard(3)} />
             </div>
             <div className="feature-card">
               <img src={BlogIcon} alt="documentos verificados" />
               <p className="feature-card-title">
                 Acesse nosso Blog
-                <br /> e <u>saiba mais</u>
+                <br /> e{" "}
+                <a href="#">
+                  <u>saiba mais</u>
+                </a>
               </p>
             </div>
           </div>
+        </section>
+        <section className="home-section-2">
+          <Input.Search
+            placeholder="Selecionar cidade"
+            enterButton
+          />
         </section>
       </main>
     </React.Fragment>
